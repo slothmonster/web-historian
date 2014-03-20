ea6fe8039151b72824274e79c0e7ae442f53ba63
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-03-20 10:51:49 -0700

    Refactor & serving CSS.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -6,13 +6,26 @@ exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  "access-control-max-age": 10 // Seconds.
+  // 'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+exports.serveAssets = function(res, asset, contentType) {
+  // gett it f/m the url somehow??
+  // contentType = asset.parseURLImMakingThisUP(".extension");
+  if(asset.indexOf(".css") !== -1) {
+    contentType = "text/css";
+  } else {
+    contentType = "text/html";
+  }
+  headers['Content-Type'] = contentType;
+  fs.readFile(asset, function(err, data){
+    if(err) {
+      //if didn't read return internal error code
+      throw err;
+    }
+    res.writeHead(200, headers);
+    // Check our handlers, ensure not returning data before file-read finishes.
+    res.end(data);
+  });
 };
-
-// As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -4,37 +4,20 @@ var httpHelps = require('./http-helpers.js');
 // require more modules/folders here!
 var fs = require('fs');
 
-
 var routes = {
+  "/styles.css": __dirname + '/public/styles.css',
   "/": __dirname + '/public/index.html',
   "/www.google.com": path.normalize(__dirname + '/../archives/sites/www.google.com')
 }
-
 
 exports.handleRequest = function (req, res) {
   var url = req.url;
   var route = routes[url];
   var data;
-  console.log("url: " + url);
-  console.log("route: " + route);
-  console.log("data: ", data);
-  
   if(route) {
-    fs.readFile(route, function(err, data){
-      if(err) {
-        //if didn't read return internal error code
-        throw err;
-      }
-      res.writeHead(200, httpHelps.headers);
-      // Check our handlers, ensure not returning data before file-read finishes.
-      res.end(data);
-    });
+    httpHelps.serveAssets(res, route);
   } else {
     res.writeHead(404, httpHelps.headers);
     res.end();
   }
 };
-
-serveRoot = function(){
-
-}
